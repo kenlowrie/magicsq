00c836f3ffae263209d73b16ae7ba5db8a013716
Ken Lowrie <klowrie@gmail.com>
2014-08-08 18:18:09 -0500

    Added the ability to have multiple jumbled sets, and tracked them as well as the original.

--- cterms.php
+++ cterms.php
@@ -23,14 +23,24 @@ class Term {
 class Terms{
 	protected $terms = array();
 	protected $filename = "<>";
+	protected $jumbled = array();
 	
     
     public function addTerm($term){
+    		//MyLog("ADDTERM: %s : %s", $term->getTerm(), $term->getDefinition());
         $this->terms[] = $term;
     }
 	
 	public function getTerms(){
 		return $this->terms;
+	}
+
+	public function getJumbledTerms(){
+		return $this->jumbled;
+	}
+	
+	public function numVariants(){
+		return count($this->jumbled);
 	}
 
 	public function setFilename($filename) {
@@ -42,15 +52,21 @@ class Terms{
 	
     public function randomizeTerms() {
         
-        //MyLog("There are %d terms in the array", count($this->terms));
-        for ($i = count($this->terms)-1; $i > 0; --$i) {
+		$copy = $this->terms;		// Make a copy of the current terms
+
+        MyLog("RANDOMIZE: There are %d terms in the array", count($copy));
+		MyPrint("Randomization in progress: ");
+        for ($i = count($copy)-1; $i > 0; --$i) {
+        		MyPrint(".");
             
-            $j = rand(0,count($this->terms)) % $i;
+            $j = rand(0,count($copy)) % $i;
             
-            $tmpTerm = $this->terms[$i];
-            $this->terms[$i] = $this->terms[$j];
-            $this->terms[$j] = $tmpTerm;
+            $tmpTerm = $copy[$i];
+            $copy[$i] = $copy[$j];
+            $copy[$j] = $tmpTerm;
         }
+		MyLog(" completed.<br />\r\nRANDOMIZE: Saving jumbled copy<br />");
+		$this->jumbled[] = $copy;		// Save this copy of the jumbled terms into the array
     }
     
     public function loadTerms($howMany) {
@@ -63,7 +79,7 @@ class Terms{
         }
     }
 	
-	public function output($ms){
+	public function output($ms,$jumbleset=0){
 		$N = $ms->getN();
 		$lines = array();
 		$count = 1;
@@ -72,26 +88,63 @@ class Terms{
 		
 		$fmt->startDiv("termtable");
 		$fmt->startTable();
-		
-		for ($X = 0; $X < $N; ++$X) {
-			for ($Y = 0; $Y < $N; ++$Y) {
-				$fmt->startRow();
-				$item = $ms->getElement($X,$Y);
-				$t1 = $this->terms[$item-1]->getTerm();
-				$d1 = $this->terms[$count-1]->getDefinition();
 
-				$fmt->writeClassData("tdterm", "%c. %s", $letter, $t1);
-				$fmt->writeClassData("tddef", "%d. %s", $count, $d1);
-				//MyLog("%c. %s&nbsp;&nbsp; -- %d. %s", $letter, $t1, $count, $d1 );
-				++$count;
-				++$letter;
-				$fmt->endRow();
+		if( $this->numVariants() > 0){
+			$js = $this->jumbled[$jumbleset];
+			
+			for ($X = 0; $X < $N; ++$X) {
+				for ($Y = 0; $Y < $N; ++$Y) {
+					$fmt->startRow();
+					$item = $ms->getElement($X,$Y);
+					$t1 = $js[$item-1]->getTerm();
+					$d1 = $js[$count-1]->getDefinition();
+	
+					$fmt->writeClassData("tdterm", "%c. %s", $letter, $t1);
+					$fmt->writeClassData("tddef", "%d. %s", $count, $d1);
+					++$count;
+					++$letter;
+					$fmt->endRow();
+				}
 			}
+		} else {
+			MyLog("output called with no variants defined...");
 		}
 		$fmt->endTable();
 		$fmt->endDiv();
 	}
     
+	public function dumpTermSet($which, $termSet){
+		$count = 1;
+		$letter = 65;
+		$fmt = new cHTMLFormatter;
+		
+		$fmt->startDiv("termtable");
+		$fmt->h3($which);
+		$fmt->startTable();
+		
+		for ($X = 0; $X < count($termSet); ++$X) {
+			$fmt->startRow();
+			$t1 = $termSet[$X]->getTerm();
+			$d1 = $termSet[$X]->getDefinition();
+
+			$fmt->writeClassData("tdterm", "%c. %s", $letter, $t1);
+			$fmt->writeClassData("tddef", "%d. %s", $count, $d1);
+			++$count;
+			++$letter;
+			$fmt->endRow();
+		}
+		$fmt->endTable();
+		$fmt->endDiv();
+	}
+	
+	public function dumpTermObject(){
+		$this->dumpTermSet("Primary Term Set", $this->terms);
+		
+		for ($X = 0; $X < $this->numVariants(); ++$X){
+			$this->dumpTermSet("Jumbled Term Set #$X", $this->jumbled[$X]);
+		}
+	}
+
     public function dumpTerms() {
         for ($i = 0; $i < count($this->terms); ++$i) {
 
