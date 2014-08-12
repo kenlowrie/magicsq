eeca022453223e63f192a672da49e1efe43f69af
Ken Lowrie <klowrie@gmail.com>
2014-08-12 16:48:10 -0500

    Added the weird file type that MS EXCEL puts on file when you export from there.

--- uploadterms.php
+++ uploadterms.php
@@ -7,26 +7,26 @@ include_once ('formatters.php');
 function uploadinputfile($fmt){
 	$allowedExts = array("txt", "csv");
 	$temp = explode(".", $_FILES["file"]["name"]);
-	$extension = end($temp);
+	$extension = end($temp);		// TODO: Should I upper case the extension???
 	
 	$fmt->startDiv("statusarea");
 	
 	$fmt->h4("Processing upload of " . $_FILES["file"]["name"] . "...");
 	// Make sure that the file is recognized...
-	if (( ($_FILES["file"]["type"] == "text/csv") || ($_FILES["file"]["type"] == "text/plain")) 
+	if (( ($_FILES["file"]["type"] == "text/csv") || ($_FILES["file"]["type"] == "text/plain") || ($_FILES["file"]["type"] == "application/vnd.ms-excel")) 
 	   && ($_FILES["file"]["size"] < 32768)
 	   && in_array($extension, $allowedExts)) {
 	   	
 		if ($_FILES["file"]["error"] > 0) {
-		  	$fmt->write("Error: " . $_FILES["file"]["error"] . "<br>");
+		  	$fmt->write("Error: " . $_FILES["file"]["error"] . "<br />");
 		} else {
-			$fmt->write("Completed upload process...");
-		  	$fmt->write("Filename: " . $_FILES["file"]["name"]);
-		  	$fmt->write("Filetype: " . $_FILES["file"]["type"]);
-		  	$fmt->write("Filesize: " . ($_FILES["file"]["size"] / 1024) . " kB");
-		  	$fmt->write("Tempname: " . $_FILES["file"]["tmp_name"]);
+			$fmt->writeAndBreak("Completed upload process...");
+		  	$fmt->writeAndBreak("Filename: " . $_FILES["file"]["name"]);
+		  	$fmt->writeAndBreak("Filetype: " . $_FILES["file"]["type"]);
+		  	$fmt->writeAndBreak("Filesize: " . ($_FILES["file"]["size"] / 1024) . " kB");
+		  	$fmt->writeAndBreak("Tempname: " . $_FILES["file"]["tmp_name"]);
 	
-			$fmt->write("<br />Parsing the CSV file into terms...");	
+			$fmt->writeAndBreak("<br />Parsing the CSV file into terms...");	
 			$row = 1;
 			$myterms = new Terms;
 			$myterms->setFilename($_FILES["file"]["name"]);
@@ -45,10 +45,13 @@ function uploadinputfile($fmt){
 			    fclose($handle);
 			}
 			setTerms($myterms);
-			$fmt->write("Parsing has completed... Found <%d> terms.",$row-1);
+			$fmt->writeRawData("Parsing has completed... Found <%d> terms.<br />",$row-1);
 		}
 	} else {
-		$fmt->write("Invalid file [%s] passed to uploaded. Must be .CSV or .TXT",$_FILES["file"]["name"]);
+		$fmt->writeRawData("Invalid file [%s] passed to uploaded. Must be .CSV or .TXT",$_FILES["file"]["name"],true,true);
+		$fmt->writeAndBreak("Filetype: " . $_FILES["file"]["type"]);
+		$fmt->writeAndBreak("Filesize: " . ($_FILES["file"]["size"] / 1024) . " kB");
+		$fmt->writeAndBreak("Tempname: " . $_FILES["file"]["tmp_name"]);
 	}
 	
 	//$fmt->addLink("mkms.php","Click me to return to the Magic Square Maker Page");
