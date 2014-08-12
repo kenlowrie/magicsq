23111b9aa1f8c753df2d3c75137771723f472745
Ken Lowrie <klowrie@gmail.com>
2014-08-12 16:46:23 -0500

    Make it so you can return strings instead of printing

--- formatters.php
+++ formatters.php
@@ -5,120 +5,144 @@ function printMagicSquare($ms){
 }
 
 class cHTMLFormatter{
+	public $justPrint=true;
+	
+	public function _print($outinfo){
+		if($this->justPrint) print($outinfo);
+		return $outinfo;
+	}
 	public function hr(){
-		print("<hr />");
+		return $this->_print("<hr />");
 	}
 	public function brk(){
-		print("<br />");
+		return $this->_print("<br />");
 	}
 	public function h1($text){
-		print("<h1>". $text . "</h1>\n");
+		return $this->_print("<h1>". $text . "</h1>\n");
 	}
 	public function h2($text){
-		print("<h2>". $text . "</h2>\n");
+		return $this->_print("<h2>". $text . "</h2>\n");
 	}
 	public function h3($text){
-		print("<h3>". $text . "</h3>\n");
+		return $this->_print("<h3>". $text . "</h3>\n");
 	}
 	public function h4($text){
-		print("<h4>". $text . "</h4>\n");
+		return $this->_print("<h4>". $text . "</h4>\n");
 	}
 	public function p($text, $class=NULL){
 		$element = IsSet($class) ? "<p class=\"$class\">" : "<p>";
-		print($element . $text . "</p>\r\n");
+		return $this->_print($element . $text . "</p>\r\n");
 	}
-	public function write($text,$newline=true){
-		print($text);
-		if ($newline) print("\n");
+	public function write($text,$newline=true,$autobreak=false){
+		$curtext = $this->_print($text);
+		if ($newline) return $curtext .= $this->_print($autobreak ? "<br />" : "" . "\n");
+		//if ($newline) print("\n");
+		return $curtext;
 	}
-	public function addLink($page,$text){
-		print("<a href=\"$page\">$text</a>");
+	public function writeAndBreak($text){
+		return $this->_print($text . "<br />\n");
+	}
+	public function addLink($page,$text,$blank=false){
+		if(!$blank){
+			return $this->_print("<a href=\"$page\">$text</a>");			
+		} else{
+			return $this->_print("<a href=\"$page\" target=\"_blank\">$text</a>");			
+		}
 	}
 
 	public function linkbutton($action, $text, $help=NULL, $method="POST", $type="submit", $name="name")
 	{
-		print("<form method=\"$method\" action=\"$action\">\n");
-		print("  <input type=\"$type\" name=\"$name\" value=\"$text\">\n");
-		print("</form>\n");
-		print($help."\n");		
+		$curtext = $this->_print("<form method=\"$method\" action=\"$action\">\n");
+		$curtext .= $this->_print("  <input type=\"$type\" name=\"$name\" value=\"$text\">\n");
+		$curtext .= $this->_print("</form>\n");
+		$curtext .= $this->_print($help."\n");	
+		return $curtext;	
 	}
 	
 	public function startDiv($id){
-		print("<div id=\"$id\">\n");
+		return $this->_print("<div id=\"$id\">\n");
 	}
 	public function startDivs($array_of_divs){
+		$curtext = "";
 		foreach ($array_of_divs as $div) {
-			$this->startDiv($div);
+			$curtext .= $this->startDiv($div);
 		}
+		return $curtext;
 	}
 	public function endDiv($count=1){
+		$curtext = "";
 		while ($count--){
-			print("</div>\n");		
+			$curtext .= $this->_print("</div>\n");		
 		}
+		return $curtext;
 	}
 
 	public function startTable(){
-		print("  <table>\n");
+		return $this->_print("  <table>\n");
 	}
 	public function endTable(){
-		print("  </table>\n");
+		return $this->_print("  </table>\n");
 	}
 
 	public function startRow(){
-		print("    <tr>\n");
+		return $this->_print("    <tr>\n");
 	}
 	public function endRow(){
-		print("    </tr>\n");
+		return $this->_print("    </tr>\n");
 	}
 
 	public function startCell($class){
 		if ($class != ""){
-			print("      <td class=\"$class\">");
+			return $this->_print("      <td class=\"$class\">");
 		} else {
-			print("      <td>");		
+			return $this->_print("      <td>");		
 		}
 	}
 	public function endCell(){
-		print("</td>\n");
+		return $this->_print("</td>\n");
 	}
-	public function writeRawData($format_string, $newline=true){
+	public function writeRawData($format_string, $newline=true, $autobreak=false){
+		$curtext = "";
 		if( func_num_args() == 1){
-			print($format_string);
+			$curtext .= $this->_print($format_string);
 		} else {
 			$args = func_get_args();
 			array_shift($args);
 			$tmp = vsprintf($format_string, $args);
-			print($tmp);	
+			$curtext .= $this->_print($tmp);	
 		}
-		if ($newline) print("\n");
+		if ($newline) $curtext .= $this->_print($autobreak ? "<br />" : "" . "\n");
+		return $curtext;
 	}
 	public function writeCellData($format_string)
 	{
-		$this->startCell("");
+		$curtext = $this->startCell("");
 		if( func_num_args() == 1){
-			print($format_string);
+			$curtext .= $this->_print($format_string);
 		} else {
 			$args = func_get_args();
 			array_shift($args);
 			$tmp = vsprintf($format_string, $args);
-			print($tmp);
+			$curtext .= $this->_print($tmp);
 		}
-		$this->endCell();	
+		$curtext .= $this->endCell();
+		return $curtext;
 	}
 	public function writeClassData($class, $format_string)
 	{
-		$this->startCell($class);
+		$curtext = $this->startCell($class);
 		if( func_num_args() == 2){
-			print($format_string);
+			$curtext .= $this->_print($format_string);
 		} else {
 			//TODO: This is an error if called with 1 arg...
 			$args = func_get_args();
 			array_shift($args);			// EAT $class
 			array_shift($args);			// EAT $format_string
 			$tmp = vsprintf($format_string, $args);
-			print($tmp);
+			$curtext .= $this->_print($tmp);
 		}
-		$this->endCell();
+		$curtext .= $this->endCell();
+		return $curtext;
 	}
 }
 
