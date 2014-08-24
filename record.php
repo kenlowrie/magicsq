cb6e1af0127fb727e837a9337e492984f19bd622
Ken Lowrie <klowrie@gmail.com>
2014-08-24 13:16:21 -0500

    Don't hardcode terms and definitions. Pass variables via post instead of get.

--- record.php
+++ record.php
@@ -27,7 +27,7 @@ function get_help($key)
           $str = "Select the input file from your computer and press submit. This must be a valid CSV file, with either .CSV or .TXT as the file extension. Each line requires two comma separated items, a term and its definition.<br /><br />e.g. \"My Term\", \"My Definition.\"";
           break;
      case 'freeterm':
-          $str = "Line number of term in the uploaded file that will be solved automatically. This will assist the student in understanding how to fill out the answer sheet. Zero (0) means <i>\"No Free Term\"</i>.";
+          $str = "Line number of item in the uploaded file that will be solved automatically. This will assist the student in understanding how to fill out the answer sheet. Zero (0) means <i>\"No Free Term\"</i>.";
           break;
      case 'alignft':
           $str = "Check to automatically align free term (see above description) to the term/definition that is aligned in the output. In each magic square, there is at least one term/definition that will be aligned. By checking here, the aligned term becomes the free term, as long as free term (above) is not 0.";
@@ -45,41 +45,46 @@ function display_quiz_form($fmt, $alias, $quiz, $commit_id)
 	//$mstype = $quiz->magicSquareSize;
 	$variants = $quiz->variants;
 	$freeterm = $quiz->freeTerm;
+	$terms = getTerms();
 
 	$fmt->startSection("loadTerms");
 
-	$fmt->h3("Terms and definitions");
+	if (IsSet($terms)){
+		$fmt->h3($terms->getHeader(1)."s and ".$terms->getHeader(2)."s");		
+	} else {
+		$fmt->h3("Terms and Definitions");		
+	}
 
 	$fmt->startDivClass("termInfo");	
-	$terms = getTerms();
 	if (IsSet($terms)){
 		$numterms = count($terms->getTerms());
-		$fmt->h5("Current term file information");
+		$fmt->h5("Current file information");
 		$fmt->startP();
-		$fmt->write("\"" . $terms->getFilename() . "\" is the currently loaded terms file.");
+		$fmt->write("\"" . $terms->getFilename() . "\" is the currently loaded file.");
 		$squaresize = sqrt($numterms);
 		if( isValidSquareSize($squaresize)){
 			$quiz->magicSquareSize = intval($squaresize);
-			$fmt->write("There are " . $numterms . " terms, which will yield a " . $quiz->magicSquareSize . "x" . $quiz->magicSquareSize . " magic square.");
+			$fmt->write("There are " . $numterms . " items, which will yield a " . $quiz->magicSquareSize . "x" . $quiz->magicSquareSize . " magic square.");
 		} else {
 			$quiz->magicSquareSize = 0;
-			$fmt->write("Based on this term set, I cannot generate a magic square for you. Only odd sized squares of 3, 5, 7 or 9 are currently supported, therefore I need a terms file with 9, 25, 49 or 81 terms in it.");
+			$fmt->write("Based on this set, I cannot generate a magic square for you. Only odd sized squares of 3, 5, 7 or 9 are currently supported, therefore I need an input file with 9, 25, 49 or 81 items in it.");
 		}
 		if (!isAppleDevice()){
-			$fmt->write("To replace this set of terms, just load another file using the button below.");
+			$fmt->write("To replace this set of items, just load another file using the button below.");
 		}
 		$fmt->endP();
 	}
 	if (!isAppleDevice()){
-		$fmt->h5("Load a new terms file");
+		$fmt->h5("Load a new input file");
 		$loadfile = LOAD_FILE;
-		$fmt->write("<form method=\"POST\" action=\"$alias?type=$loadfile\" enctype=\"multipart/form-data\">");
+		$fmt->write("<form method=\"POST\" action=\"$alias\" enctype=\"multipart/form-data\">");
 		$fmt->write("<input type=\"file\" name=\"file\" class=\"fancyButton loadTermsButton\">");
 		$fmt->brk();
-		$fmt->write("<input type=\"submit\" name=\"submit\" value=\"Choose Terms File and Click Here to Upload\" class=\"fancyButton loadTermsButton\">");
+		$fmt->write("<input type=\"submit\" name=\"submit\" value=\"Choose Input File and Click Here to Upload\" class=\"fancyButton loadTermsButton\">");
+		$fmt->write("<input type=\"hidden\" name=\"type\" value=\"$loadfile\">");
 		$fmt->write("</form>");		
 	} else {
-		$fmt->write("Uploading terms with iOS devices is not supported at this time. Use the Generate buttons below to test.");		
+		$fmt->write("Uploading files with iOS devices is not supported at this time. Use the Generate buttons below to test.");		
 	}
 
 	$fmt->endDiv();
@@ -100,6 +105,7 @@ function display_quiz_form($fmt, $alias, $quiz, $commit_id)
 		
 	     // construct the form, and then the outer table that will hold the field definitions
 		$fmt->write("<form method=\"POST\" action=\"makequiz.php\">");
+		//$fmt->write("")
 
 		$fmt->startDivClass($divOptionOddEven[++$optionCounter % 2]);	
 		$fmt->startDivClass($divMainLeft);
@@ -120,7 +126,7 @@ function display_quiz_form($fmt, $alias, $quiz, $commit_id)
 		$fmt->startDivClass($divMainLeft);
 			
 		$fmt->h4("Number of variants");
-	    $fmt->write("<input type=\"number\" min=\"1\" max=\"9\" name=\"variants\" value=\"$variants\"><br /><br />");
+	    $fmt->write("<input type=\"number\" min=\"1\" max=\"99\" name=\"variants\" value=\"$variants\"><br /><br />");
 	
 		$fmt->endDiv();
 		$fmt->startDivClass($divMainRight);
@@ -145,12 +151,12 @@ function display_quiz_form($fmt, $alias, $quiz, $commit_id)
 		if ($freeterm > 0){
 			if($freeterm < $maxterms){
 				$termlist = $terms->getTerms();
-				$fmt->write("Based on the currently loaded term set, the free term will be \"".$termlist[$freeterm-1]->getTerm() . "\".");				
+				$fmt->write("Based on the currently loaded set, the free term will be \"".$termlist[$freeterm-1]->getTerm() . "\".");				
 			} else{
-				$fmt->write("The currently selected free term is not within the limits of the current term set.");
+				$fmt->write("The currently selected free term is not within the limits of the current set.");
 			}
 		} else {
-			$fmt->write("There will be no free term given, probably because you're a mean teacher or you have mean students...");		
+			$fmt->write("There will be no free term in the output PDFs.");		
 		}
 		
 		$fmt->endDiv(2);
@@ -186,11 +192,17 @@ function display_quiz_form($fmt, $alias, $quiz, $commit_id)
 
 	$fmt->startSection("helpers");
 	
-	$fmt->linkbutton("$alias?type=".MAKE_TERMS."&size=3", "Generate 3x3 Term Set",NULL,"fancyButton helperButton");
-	$fmt->linkbutton("$alias?type=".MAKE_TERMS."&size=5", "Generate 5x5 Term Set",NULL,"fancyButton helperButton");
-	$fmt->linkbutton("$alias?type=".MAKE_TERMS."&size=7", "Generate 7x7 Term Set",NULL,"fancyButton helperButton");
-	$fmt->linkbutton("$alias?type=".RESET, "Reset and Start Over",NULL,"fancyButton helperButton");
-	$fmt->linkbutton("$alias", "Refresh This Page",NULL,"fancyButton helperButton");
+	$hiddenVars = array( 
+		array("type" => MAKE_TERMS, "size" => 3),
+		array("type" => MAKE_TERMS, "size" => 5),
+		array("type" => MAKE_TERMS, "size" => 7),
+		array("type" => RESET),
+	);
+	$fmt->linkbutton($alias, "Generate 3x3 Term Set",NULL,"fancyButton helperButton",$hiddenVars[0]);
+	$fmt->linkbutton($alias, "Generate 5x5 Term Set",NULL,"fancyButton helperButton",$hiddenVars[1]);
+	$fmt->linkbutton($alias, "Generate 7x7 Term Set",NULL,"fancyButton helperButton",$hiddenVars[2]);
+	$fmt->linkbutton($alias, "Reset and Start Over",NULL,"fancyButton helperButton",$hiddenVars[3]);
+	$fmt->linkbutton($alias, "Refresh This Page",NULL,"fancyButton helperButton");
 
 	$fmt->endSection();
 }
