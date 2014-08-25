e203d72758708381ae1e7ef0b9945dcebc07f7a1
Ken Lowrie <klowrie@gmail.com>
2014-08-25 13:55:18 -0500

    Added parseTerms to handle parsing of the input data being manually entered.

--- utility.php
+++ utility.php
@@ -64,4 +64,43 @@ function endsWith($haystack, $needle)
     return (substr($haystack, -strlen($needle)) === $needle);
 }
 
+function parseTerms($fmt,$inputData){
+	$row = 1;
+	$headers = 0;		// ignore header rows in the final count
+	$myterms = new Terms;
+	$myterms->setFilename("Manual Entry.csv");
+
+	$fmt->startDiv("statusArea");
+	
+	$fmt->h4("Processing manual entry of terms...");
+	// Make sure that the file is recognized...
+
+	foreach ($inputData as $line){
+		$data = str_getcsv($line);
+		
+        $num = count($data);
+		
+		if ($num == 1){
+			// This is usually because of a blank line. Ignore it.
+			continue;
+		}
+		else if ($num != 2){
+			$fmt->p("Error on line $row: Found $num terms, expected 2.");
+		} else {
+			if (startsWith($data[0],"+++")){
+				$myterms->setHeaders(substr($data[0],3),$data[1]);
+				++$headers;
+				$fmt->p("New column headers defined: Column 1: \"".$myterms->getHeader(1)."\" Column 2: \"".$myterms->getHeader(2)."\"");
+			} else{
+				$myterms->addTerm(new Term($data[0], iconv('UTF-8', 'ASCII//TRANSLIT', $data[1])));
+			}
+		}
+        $row++;
+    }
+	setTerms($myterms);
+	$fmt->writeRawData("Parsing has completed... Found <".strval($row-1-$headers)."> terms.<br />");
+
+	$fmt->endDiv();
+}
+
 ?>
