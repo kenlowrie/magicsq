2512f12c7504c661d97f359cb797e8a27f57ebfe
Ken Lowrie <klowrie@gmail.com>
2014-09-10 17:57:51 -0500

    The dynamic replacement of content is now working.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 "use strict";
 
 function prepareMKMS() {
-	document.getElementById("selectOptions").onclick = function() {
+	document.getElementById("quizHelp").onclick = function() {
 		// if (document.getElementById("selectOptions").style.display === "none"){
 			// document.getElementById("selectOptions").style.display = "block";
 		// } else{
@@ -20,26 +20,55 @@ function prepareMKMS() {
 	$(".quizOptionRight").hide();
 }
 
+function isTextNode(){
+	return (this.nodeType === 3);
+}
 
-function prepareMAKEQUIZ() {
-	console.log('inside prepareMAKEQUIZ()');
+function resetDocumentData(pvID,object,key,value){
+	var mynode = $(pvID).children("div")[0];
+	//var curNotes = $(pvID).find(".magicSquareNotes").html();
+	switch(key){
+		case "notes":
+			var decoded = $('<textarea/>').html(value).val();
+			$(pvID).find(".magicSquareNotes").html(decoded);		
+			break;
+		case "square":
+			var decoded = $('<textarea/>').html(value).val();
+			$(pvID).find(".puzzleSquare").html(decoded);	
+			console.log($(pvID).find(".puzzleSquare").html());
+			break;
+		case "puzzle":
+			var decoded = $('<textarea/>').html(value).val();
+			$(pvID).find(".puzzleTerms").html(decoded);		
+			break;
+		default:
+			break;
+	}
+}
+
+function regenPuzzleObject(variant,object,pvID){
+	console.log('inside prepareMAKEQUIZ('+variant+','+object+','+pvID+')');
+	var jqxhr = $.getJSON('msjsonp.php?callback=?','variant='+variant+'&item='+object,function(data){
+    		console.log('success:');
+    		$.each(data, function( key, val) {
+    			console.log(key + "=" + val);
+	    		resetDocumentData(pvID,object,key,val);			
+    		});
+	})
+	.done(function() {
+		//console.log("second success");
+	})
+	.fail(function() {
+		console.log("error ");
+	})
+	.always(function() {
+		//console.log("complete");
+	});
+}
+
+function prepareMAKEQUIZ(variant,object) {
 	document.getElementById("myElement").onclick = function() {
-		var jqxhr = $.getJSON('msjsonp.php?callback=?','variant=1&item=ms',function(data){
-	    		console.log('success:');
-	    		$.each(data, function( key, val) {
-	    			console.log(key + "=" + val);
-	    		});
-		})
-		.done(function() {
-			console.log("second success");
-		})
-		.fail(function() {
-			console.log("error ");
-		})
-		.always(function() {
-			console.log("complete");
-		});
-		//console.log("from jqxhr: " + jqxhr.statusText);
+		regenPuzzleObject(0,1,'myid');
 	};
 }
 
