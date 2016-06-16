3890ed803ae7c3c50eef5c18d5013424cf2df787
Ken Lowrie <klowrie@gmail.com>
2016-06-15 20:01:11 -0500

    Add GNU Licensing Information
    Added GNU Licensing Information to source files.

--- msjsonp.php
+++ msjsonp.php
@@ -1,4 +1,21 @@
 <?php
+// This file is part of the Magic Square Quiz Maker (magicsq) application.
+// Copyright (C) 2014-2016 Ken Lowrie
+//
+// magicsq is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// magicsq is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// See LICENSE.TXT for more information.   
 
 include_once ('session.php');	// session handling code
 include_once ('datautil.php');
